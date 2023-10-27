1607e5f5b957885c61dae83ae81e6877e46eb43c
pathaweeC <pathaweechayakul@gmail.com>
2023-10-27 17:27:49 +0700

    Test

--- CCTV_Security/Program.cs
+++ CCTV_Security/Program.cs
@@ -1,97 +1,70 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 class Program
 {
-    private static async Task Main(string[] args)
+    private const string LineAccessToken = "JAxPFavKxgi07CLsN2eE29Li09eXa6Ab7ncHikkFAtQ";
+    private static DateTime lastUpdateTime = DateTime.MinValue;
+
+    static async Task Main(string[] args)
     {
+        Console.WriteLine("Waiting for new images in the folder...");
+
+        // Get Current Date
         DateTime currentDate = DateTime.Now;
         string Date = currentDate.ToString("yyyy-MM-dd");
-
         string localDirectory = "@/data/MotionDetect/" + Date + "";
-
-        string lineNotifyToken = "JAxPFavKxgi07CLsN2eE29Li09eXa6Ab7ncHikkFAtQ";
-
-        if (!Directory.Exists(localDirectory))
-        {
-            Directory.CreateDirectory(localDirectory);
-        }
 
         while (true)
         {
-            try
+            string latestImage = GetLatestImage(localDirectory);
+
+            if (!string.IsNullOrEmpty(latestImage) && File.GetLastWriteTime(latestImage) > lastUpdateTime)
             {
-                DateTime lastUpdateTime = DateTime.MinValue;
-                // Get Current Date
-
-                string latestImage = GetLatestImage(localDirectory);
-
-                if (!string.IsNullOrEmpty(latestImage) && File.GetLastWriteTime(latestImage) > lastUpdateTime)
-                {
-                    // Send the latest image to Line
-                    await SendPictureToLineNotifyAsync(latestImage, "Motion Detected");
-                    lastUpdateTime = File.GetLastWriteTime(latestImage);
-                }
-
-                // Sleep for a period before checking again (e.g., every 5 seconds)
-                await Task.Delay(TimeSpan.FromSeconds(5));
-
-                /////////// send pic to line noti
-                //SendPictureToLineNotifyAsync(latestImage, "Motion Detect").Wait();
+                // Send the latest image to Line Notify with a message
+                await SendPictureToLineNotifyAsync(latestImage, "Motion Detected");
+                lastUpdateTime = File.GetLastWriteTime(latestImage);
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error: {ex.Message}");
-            }
+
+            // Sleep for a period before checking again (e.g., every 5 seconds)
+            await Task.Delay(TimeSpan.FromSeconds(5));
+        }
+    }
+
+    static string GetLatestImage(string folderPath)
+    {
+        string[] imageFiles = Directory.GetFiles(folderPath, "*.jpg");
+
+        if (imageFiles.Length == 0)
+        {
+            return null;
         }
 
-        // line noti sender task
-        async Task SendPictureToLineNotifyAsync(string imagePath, string message)
+        Array.Sort(imageFiles, (a, b) => File.GetLastWriteTime(b).CompareTo(File.GetLastWriteTime(a)));
+
+        return imageFiles[0];
+    }
+
+    static async Task SendPictureToLineNotifyAsync(string imagePath, string message)
+    {
+        using (var httpClient = new HttpClient())
         {
-            using (var httpClient = new HttpClient())
+            var content = new MultipartFormDataContent();
+            content.Add(new StreamContent(File.OpenRead(imagePath)), "imageFile", Path.GetFileName(imagePath));
+            content.Add(new StringContent(message), "message");
+
+            var response = await httpClient.PostAsync($"https://notify-api.line.me/api/notify?token={LineAccessToken}", content);
+
+            if (!response.IsSuccessStatusCode)
             {
-                var content = new MultipartFormDataContent();
-                content.Add(new StreamContent(File.OpenRead(imagePath)), "imageFile", "image.jpg");
-                content.Add(new StringContent(message), "message");
-
-                var request = new HttpRequestMessage
-                {
-                    Method = HttpMethod.Post,
-                    RequestUri = new Uri("https://notify-api.line.me/api/notify"),
-                    Content = content
-                };
-
-                request.Headers.Add("Authorization", "Bearer " + lineNotifyToken);
-
-                var response = await httpClient.SendAsync(request);
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    string responseContent = await response.Content.ReadAsStringAsync();
-                    Console.WriteLine($"Line Notify request failed with status code {response.StatusCode}");
-                    Console.WriteLine($"Response content: {responseContent}");
-                }
-                else
-                {
-                    Console.WriteLine("Picture sent to Line Notify successfully!");
-                }
+                Console.WriteLine("Failed to send Line Notify message.");
             }
-        }
-
-        static string GetLatestImage(string folderPath)
-        {
-            string[] imageFiles = Directory.GetFiles(folderPath, "*.jpg");
-
-            if (imageFiles.Length == 0)
+            else
             {
-                return null;
+                Console.WriteLine("Line Notify message sent successfully.");
             }
-
-            Array.Sort(imageFiles, (a, b) => File.GetLastWriteTime(b).CompareTo(File.GetLastWriteTime(a)));
-
-            return imageFiles[0];
         }
     }
 }
