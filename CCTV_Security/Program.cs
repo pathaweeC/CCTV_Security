c166fdf3a581076ec34e4d2d84ca5b12329fa754
pathaweeC <pathaweechayakul@gmail.com>
2023-10-27 17:55:36 +0700

    Test

--- CCTV_Security/Program.cs
+++ CCTV_Security/Program.cs
@@ -20,15 +20,15 @@ class Program
             Console.WriteLine("Waiting for new images in the folder...");
             string latestImage = GetLatestImage(localDirectory);
 
-            //if (!string.IsNullOrEmpty(latestImage) && File.GetLastWriteTime(latestImage) > lastUpdateTime)
-            //{
+            if (!string.IsNullOrEmpty(latestImage) /*&& File.GetLastWriteTime(latestImage) > lastUpdateTime*/)
+            {
                 // Send the latest image to Line Notify with a message
                 await SendPictureToLineNotifyAsync(latestImage, "Motion Detected");
                 lastUpdateTime = File.GetLastWriteTime(latestImage);
-            //}
+            }
 
-            // Sleep for a period before checking again (e.g., every 5 seconds)
-            await Task.Delay(TimeSpan.FromSeconds(5));
+        // Sleep for a period before checking again (e.g., every 5 seconds)
+        await Task.Delay(TimeSpan.FromSeconds(5));
         }
     }
 
